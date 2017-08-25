a7c7ebc4f568c8e8d7124fde7e61de8aac7ba44b
shanqq <30663014+shanqq@users.noreply.github.com>
2017-08-25 20:49:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/NARUTO',function(req,res){
+   res.send('NARUTO THE SEVENTH HOKAGE OF THE KONOHA VILLAGE'); 
+});
+
+app.get('/SASUKE',function(req,res){
+    res.send('SASUKE THE ONLY UCHICHA ALIVE AFTER ITACHI KILLED ALL THE MEMBER OF THE UCHICHA');
+});
+
+app.get('/BORUTO',function(req,res){
+  res.send('BORUTO THE SON OF THE SEVENTH HOKAGE OF KONOHA VILLAGE');
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
