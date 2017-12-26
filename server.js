e144b704c28743e5f1bd0940cc38611ad50f6671
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-27 00:52:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get(function(req,res){
+app.get('/articletwo',function(req,res){
     res.File(path.join(__dirname,'ui','articletwo.html'));
 });
 app.get('/articleone',function(req,res){
