1c7db2c607c3ed8420a20783c59a968e32da2bc4
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-26 23:26:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,42 @@
-console.log('Loaded!');
+
+<script>
+
+<script>
+
+window.onscroll = function(){scrollfunction()};
+function scrollfunction()
+{
+ if (document.body.scrollTop >30 || document.documentElement.scrollTop >30)
+{
+ 	document.getElementById("naav").style.top="0";
+
+}
+else {
+document.getElementById("naav").style.top="-50px";
+}
+
+}
+
+</script>
+
+
+<script>
+var value=0;
+carousel();
+function carousel(){
+
+var i;
+var x=document.getElementsByClassName("slides");
+for(i=0;i<x.length;i++)
+{
+x[i].style.display="none";
+
+}
+value++;
+if (value> x.length) {value = 1} 
+    x[value-1].style.display = "block"; 
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+</script>
+</script>
