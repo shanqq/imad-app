3b26d2fb249c2e15988ddc05640062eaee1fc20a
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-28 01:06:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,40 @@
+
+var button=document.getElementById("counter");
+var counter=0;
+button.onclick()=function (){
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=(counter.toString());
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*window.onscroll = function(){scrollfunction()};
 
 function scrollfunction()
