8da0baddaef314d91ba64ea2ce2a9524e594915a
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-28 01:48:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,22 @@ var button=document.getElementById("counter");
 var counter=0;
 
 button.onclick=function (){
+    //create a request
+    var request = new XMLHttpRequest(); 
     
     //
+    reuest.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+            var counter=request.responseText;
+            var span=document.getElementById("total");
+            span.innerHTML=counter.toString();
+            }
+        }
+    }
+    //render the counter variable
     counter=counter+1;
     var span=document.getElementById("total");
     span.innerHTML=counter.toString();
