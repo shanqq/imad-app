db5e88998d5844c3a4d852dff330d9d73d72fdc8
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-28 02:33:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/heroacademia',function(req,res){
    res.sendFile(path.join(__dirname,'ui','heroacademia.html'));
 });
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
     var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
