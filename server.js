bd1c93e81a3d6e435344f4b7e3e026dfbd96284d
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-27 11:06:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,10 +78,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articlesName]));
-});
+
 
 app.get('/heroacademia',function(req,res){
    res.sendFile(path.join(__dirname,'ui','heroacademia.html'));
