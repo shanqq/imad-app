40997ad4f2e2523279c30501b472c2740fe8fce0
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-31 01:15:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database:'postgres',
     host:'root',
     port:'5432',
-    password:process.env.DB_PASSWORD
+   // password:process.env.DB_PASSWORD
     };
 var app = express();
 app.use(morgan('combined'));
