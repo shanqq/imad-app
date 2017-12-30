2aab81aa9d6ad6a79d97d4700ac503ad7e855679
shanqq <30663014+shanqq@users.noreply.github.com>
2017-12-31 01:35:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user:'ansarishaquib25',
     database:'ansarishaquib25',
     host:'db.imad.hasura-app.io',
-    port:'5432',
+    port:'80',
    password:process.env.DB_PASSWORD
     };
 var app = express();
